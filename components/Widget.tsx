0c1a3fa2638496d3ff8a1114b59ee2d1d5198918
Chibuzor <chibuzorm26@example.com>
2022-10-11 17:47:30 +0100

    Initial commit

---
+++ components/Widget.tsx
@@ -0,0 +1,30 @@
+import { SearchIcon } from "@heroicons/react/outline";
+import React from "react";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
+function Widget() {
+  return (
+    <div className="mt-2 px-2 col-span-2 hidden lg:inline">
+      {/* search */}
+      <div
+        className="flex items-center space-x-2 bg-gray-100 p-3 
+      rounded-full mt-2 mb-2"
+      >
+        <SearchIcon className="h-5 w-5 text-gray-400" />
+        <input
+          className="bg-transparent flex-1 outline-none"
+          type="text"
+          placeholder="Search Twitter"
+        />
+      </div>
+ 
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="iJustine"
+          options={{ height: 1000 }}
+        />
+     
+    </div>
+  );
+}
+
+export default Widget;
